43ca8fba4354fefd5d902776b8810aea74a9157e
Diegobvl <diegobarbosavl@gmail.com>
2020-09-23 15:50:50 -0300

    feat[US005] Botão editar cadastro de usuário

--- php/submitPerfil.php
+++ php/submitPerfil.php
@@ -5,32 +5,12 @@ include 'conexao.php';
 $idUsuario = $_SESSION['idusuario'];  
 $nome =$_POST['nome']; 
 $sexo =$_POST['sexo']; 
-$hestudoinicio =$_POST['hestudoinicio']; 
-$hestudofim =$_POST['hestudofim']; 
 $hsonoinicio =$_POST['hsonoinicio']; 
 $hsonofim =$_POST['hsonofim']; 
-$htrabalhoinicio =$_POST['htrabalhoinicio']; 
-$htrabalhofim =$_POST['htrabalhofim']; 
-$hlazerinicio =$_POST['hlazerinicio'];
-$hlazerfim =$_POST['hlazerfim'];
 
-
-
-$stmt = $conexao->prepare("update usuario set nome='$nome',sexo='$sexo',h_estudo_inicio='$hestudoinicio',h_estudo_fim='$hestudofim',h_sono_inicio='$hsonoinicio',h_sono_fim='$hsonofim',h_trabalho_inicio='$htrabalhoinicio',h_trabalho_fim='$htrabalhofim',h_lazer_inicio='$hlazerinicio',h_lazer_fim='$hlazerfim' where id='$idUsuario'");
+$stmt = $conexao->prepare("update usuario set nome='$nome',sexo='$sexo',h_sono_inicio='$hsonoinicio',h_sono_fim='$hsonofim' where id='$idUsuario'");
 
 $stmt->execute();
 
 header("Location: ../web/timeline.php");
-
-
-
-
-
-
-
-
-
-
-
-
  ?>
