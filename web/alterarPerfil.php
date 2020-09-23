e5b76a19c1bf2fb89ed8ba14b384595e629e3620
Diegobvl <diegobarbosavl@gmail.com>
2020-09-23 15:47:12 -0300

    refactor[US005] Indentação

--- web/alterarPerfil.php
+++ web/alterarPerfil.php
@@ -3,26 +3,20 @@
 <head>
 <?php session_start();
 
- include '../import/alterarPerfil.html';?>	
-
- 
-
+	include '../import/alterarPerfil.html';?>
 
 	<title>Alterar Perfil</title>
 </head>
 <body>
 <?php
-
- include 'menu.php';?>
+ include 'menu.php';
+ ?>
 
 <!-- Pag -->
- <main class="page-content">
-    <div class="container-fluid">
-
-  <?php include '../php/alterarPerfil.php'; ?>
-
-  </main>
- 
+<main class="page-content">
+	<div class="container-fluid">
+	<?php include '../php/alterarPerfil.php'; ?>
+</main>
 </div>
 </body>
 </html>
