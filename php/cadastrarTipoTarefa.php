d0fadceb8c1a732023908f5c4ac4a150e28bb85e
zuma <zuamirgutemberg@gmail.com>
2020-09-27 02:11:31 -0300

    fix[US006] Corrige nomenclatura das variaveis e add param liga-desliga

--- php/cadastrarTipoTarefa.php
+++ php/cadastrarTipoTarefa.php
@@ -9,13 +9,15 @@ session_start();
 $idUsuario = $_SESSION['idusuario'];  
 
 $descricao =$_POST['descricao'];
+$hInicio =$_POST['h_inicio'];
+$hFim =$_POST['h_fim'];
 
-
-
-$stmt = $conexao->prepare("insert into tipo_tarefa(usuario,descricao) values (?,?)");
+$stmt = $conexao->prepare("INSERT INTO tipo_tarefa (usuario, descricao, h_inicio, h_fim, liga_desliga) VALUES (?, ?, ?, ?, '0')");
 
 $stmt -> bindParam(1,$idUsuario);
 $stmt -> bindParam(2,$descricao);
+$stmt -> bindParam(3,$hInicio);
+$stmt -> bindParam(4,$hFim);
 
 $stmt->execute();
 //$idTarefa = $conexao->lastInsertId();
