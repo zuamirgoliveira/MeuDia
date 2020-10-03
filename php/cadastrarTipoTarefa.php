6bf145b6cc51485c0d097b31b2969f7ef9418111
Diegobvl <diegobarbosavl@gmail.com>
2020-10-02 23:09:02 -0300

    feat[US018] Validação do formulário
    Validar se for preenchido, se possui algum caractere especial e limitar o tamanho do texto.

--- php/cadastrarTipoTarefa.php
+++ php/cadastrarTipoTarefa.php
@@ -1,4 +1,3 @@
-
 <?php 
 
 include 'conexao.php';
@@ -7,27 +6,35 @@ try{
 session_start();
 
 $idUsuario = $_SESSION['idusuario'];  
-
 $descricao =$_POST['descricao'];
 $h_inicio =$_POST['h_inicio'];
 $h_fim =$_POST['h_fim'];
 
-$stmt = $conexao->prepare("insert into tipo_tarefa(usuario,descricao,h_inicio,h_fim) values (?,?,?,?)");
+if (preg_match('/^[a-zA-Z0-9]+/', $descricao) == 1 && 
+	preg_match('/^[0-9]+/', $h_inicio) == 1 && 
+	preg_match('/^[0-9]+/', $h_fim) == 1) {
 
-$stmt -> bindParam(1,$idUsuario);
-$stmt -> bindParam(2,$descricao);
-$stmt -> bindParam(3,$h_inicio);
-$stmt -> bindParam(4,$h_fim);
-$stmt->execute();
-//$idTarefa = $conexao->lastInsertId();
+	$stmt = $conexao->prepare("
+							INSERT INTO
+							   tipo_tarefa(usuario, descricao, h_inicio, h_fim) 
+							VALUES
+							   (?,?,?,?)");
 
-
-
-header("Location: ../web/tipoTarefas.php");
-
+	$stmt -> bindParam(1,$idUsuario);
+	$stmt -> bindParam(2,$descricao);
+	$stmt -> bindParam(3,$h_inicio);
+	$stmt -> bindParam(4,$h_fim);
+	
+	$stmt->execute();
+	
+	//$idTarefa = $conexao->lastInsertId();
+	
+	header("Location: ../web/tipoTarefas.php");
+}else{
+	header('Location: ../web/registrarTipoTarefa.php');
+}
 }catch(PDOException $e){
 echo 'ERROR: ' . $e->getMessage();
 }
 
-
- ?>
+?>
