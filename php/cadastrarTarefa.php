56a4bce8a48e5ceebb528f712c33682e8a6d61ab
Diegobvl <diegobarbosavl@gmail.com>
2020-10-02 22:00:35 -0300

    feat[US017] Validação do formulário
    Validar se for preenchido, se possui algum caractere especial e limitar o tamanho do texto.

--- php/cadastrarTarefa.php
+++ php/cadastrarTarefa.php
@@ -1,4 +1,3 @@
-
 <?php 
 
 include 'conexao.php';
@@ -6,8 +5,8 @@ include 'conexao.php';
 try{
 session_start();
 
-$idUsuario = $_SESSION['idusuario'];  
-$titulo =  $_POST['titulo'];
+$idUsuario =$_SESSION['idusuario'];  
+$titulo = $_POST['titulo'];
 $subtitulo =$_POST['subtitulo'];
 $descricao =$_POST['descricao'];
 $hinicio =$_POST['hinicio']; 
@@ -15,56 +14,58 @@ $hfim =$_POST['hfim'];
 $tipotarefa =$_POST['tipotarefa']; 
 $prioridade =$_POST['prioridade'];
 
+if (preg_match('/^[a-zA-Z0-9]+/', $titulo) == 1 && 
+	preg_match('/^[a-zA-Z0-9]+/', $subtitulo) == 1 && 
+	preg_match('/^[a-zA-Z0-9]+/', $descricao) == 1 && 
+	preg_match('/^[0-9]+/', $hinicio) == 1 && 
+	preg_match('/^[0-9]+/', $hfim) == 1 && 
+	preg_match('/^[a-zA-Z0-9]+/', $tipotarefa) == 1 && 
+	preg_match('/^[a-zA-Z0-9]+/', $prioridade) == 1) {
+	
+	$stmt = $conexao->query("
+						SELECT
+						  * 
+						FROM
+						  usuario 
+						WHERE
+						  id = '$idUsuario'");						  
+	$contagem = $stmt->rowCount();
+	
+	if($contagem == 1){
 
-$stmt = $conexao->query("select h_estudo_inicio,h_estudo_fim,h_sono_inicio,h_sono_fim,h_trabalho_inicio,h_trabalho_fim,h_lazer_inicio,h_lazer_fim from usuario where id='$idUsuario'");
-		$contagem = $stmt->rowCount();
-		if($contagem == 1){
+		$resultado = $stmt->fetchAll();
 
-			$resultado = $stmt->fetchAll();
+		foreach($resultado as $linha){
 
-			foreach($resultado as $linha){
+			if(is_null($linha["h_sono_inicio"]) and is_null($linha["h_sono_fim"]) ){
 
-				if(is_null($linha["h_sono_inicio"]) and is_null($linha["h_sono_fim"]) ){
+				// redirecionar para pag de perfil, completar algumas informações
 
-						// redirecionar para pag de perfil, completar algumas informações
+			}else{
+						$stmt = $conexao->prepare("insert into tarefa(usuario,titulo,subtitulo,descricao,h_inicio,h_fim,tipo_tarefa,prioridade) values (?,?,?,?,?,?,?,?)");
 
-				}else{
+						$stmt -> bindParam(1,$idUsuario);
+						$stmt -> bindParam(2,$titulo);
+						$stmt -> bindParam(3,$subtitulo);
+						$stmt -> bindParam(4,$descricao);
+						$stmt -> bindParam(5,$hinicio);
+						$stmt -> bindParam(6,$hfim);
+						$stmt -> bindParam(7,$tipotarefa);
+						$stmt -> bindParam(8,$prioridade);
 
-							$stmt = $conexao->prepare("insert into tarefa(usuario,titulo,subtitulo,descricao,h_inicio,h_fim,tipo_tarefa,prioridade) values (?,?,?,?,?,?,?,?)");
+						$stmt->execute();
+						//$idTarefa = $conexao->lastInsertId();
 
-							$stmt -> bindParam(1,$idUsuario);
-							$stmt -> bindParam(2,$titulo);
-							$stmt -> bindParam(3,$subtitulo);
-							$stmt -> bindParam(4,$descricao);
-							$stmt -> bindParam(5,$hinicio);
-							$stmt -> bindParam(6,$hfim);
-							$stmt -> bindParam(7,$tipotarefa);
-							$stmt -> bindParam(8,$prioridade);
-
-							$stmt->execute();
-							//$idTarefa = $conexao->lastInsertId();
-
-
-
-							header("Location: ../web/tarefas.php");
-
-
-
-						}
-
-
+						header("Location: ../web/tarefas.php");
 			}
-
-
-
 		}
-
-
-
+	}
+}else{
+	header('Location: ../web/registrarTarefa.php');
+}
 
 }catch(PDOException $e){
 echo 'ERROR: ' . $e->getMessage();
 }
 
-
- ?>
+?>
---
+++ php/validaCadastrarTarefa.php
@@ -0,0 +1,75 @@
+<?php 
+
+echo"<form class='form-horizontal' id='form' action='../php/cadastrarTarefa.php' method='POST'>
+<fieldset>
+
+<!-- Título-->
+<div class='form-group'>
+	<label class='col-md-4 control-label' for='titulo'>Título</label>  
+	<div class='col-md-4'>
+		<input id='titulo' name='titulo' type='text' pattern='^[a-z A-Z 0-9 Á-ú]+$' title='Apenas letras e números.' placeholder='' class='form-control input-md' maxlength='30' required=''>		
+	</div>
+</div>
+
+<!-- Subtitulo -->
+<div class='form-group'>
+	<label class='col-md-4 control-label' for='subtitulo'>Subtitulo</label>  
+	<div class='col-md-4'>
+		<input id='subtitulo' name='subtitulo' type='text' pattern='^[a-z A-Z 0-9 Á-ú]+$' title='Apenas letras e números.' placeholder='' class='form-control input-md' maxlength='50' required=''>
+	</div>
+</div>
+
+<!-- Descrição -->
+<div class='form-group'>
+	<label class='col-md-4 control-label' for='descricao'>descricao</label>
+	<div class='col-md-4'>                     
+		<textarea class='form-control' id='descricao' name='descricao' title='Apenas letras e números.' maxlength='300'></textarea>
+	</div>
+</div>
+
+<!-- Horário de Início-->
+<div class='form-group'>
+	<label class='col-md-4 control-label' for='hinicio'>Horário de Início</label>  
+	<div class='col-md-1'>
+		<input id='hinicio' name='hinicio' type='time' pattern='^[0-9]+$' title='Apenas números.' placeholder='' class='form-control input-md' required=''>
+	</div>
+</div>
+
+<!-- Horário de Termino -->
+<div class='form-group'>
+	<label class='col-md-4 control-label' for='hfim'>Horário de Termino</label>  
+	<div class='col-md-1'>
+		<input id='hfim' name='hfim' type='time' pattern='^[0-9]+$' title='Apenas números.' placeholder='' class='form-control input-md'>
+	</div>
+</div>
+
+<!-- Tipo de Tarefa -->
+<div class='form-group'>
+	<label class='col-md-4 control-label' for='tipotarefa'>Tipo de Tarefa</label>
+	<div class='col-md-3'>
+		<select id='tipotarefa' name='tipotarefa' class='form-control'>";
+		include '../php/selectTipoTarefa.php';
+	echo"</select>
+	</div>
+</div>
+
+<!-- Prioridade -->
+<div class='form-group'>
+	<label class='col-md-4 control-label' for='prioridade'>Prioridade</label>
+	<div class='col-md-3'>
+		<select id='prioridade' name='prioridade' class='form-control'>";
+		include '../php/selectPrioridade.php';
+	echo"</select>
+	</div>
+</div>
+
+<!-- Registrar -->
+<div class='form-group'>
+	<div class='col-md-4'>
+		<button id='singlebutton' name='singlebutton' class='btn btn-success'>Cadastrar</button>
+	</div>
+</div>
+</fieldset>
+</form>";
+
+?>
