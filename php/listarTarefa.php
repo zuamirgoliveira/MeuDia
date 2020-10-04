47b240ff45bf746c665a5b100a4f6e51384e1859
zuma <zuamirgutemberg@gmail.com>
2020-10-04 00:25:59 -0300

    feat[US009] Implementa modal visualizar tarefa

--- php/listarTarefa.php
+++ php/listarTarefa.php
@@ -24,7 +24,7 @@ if($contagem >= 1){
 $resultado = $stmt->fetchAll();
 
 foreach($resultado as $linha){
-        
+  $linha['json'] = json_encode($linha);        
         echo"
 			<tr style='text-align:center;'>
                 <td style='white-space: nowrap; text-align:left;'>".$linha['titulo']."</td>
@@ -54,9 +54,9 @@ foreach($resultado as $linha){
                 echo"
                 <td style='white-space: nowrap;'>
                   <center>
-                    <button class='btn btn-primary' style='margin-left: 8px' style='margin-right: 15px;'><i class='fa fa-search'></i></button>
-                    <a class='btn btn-primary' style='margin-left: 8px' style='margin-right: 10px;' href='alterarTarefa.php?id=".$linha['id']."'><div class='fa fa-edit'></div></a>
-                    <button class='btn btn-danger' style='margin-left: 8px' style='margin-right: 10px;'><a class='fa fa-trash' href='../php/excluirTarefa.php?id=".$linha['id']."'></a></button>
+                    <button id='btModal' type='button' class='btn btn-primary' style='margin-left: 8px; margin-right: 15px;' onclick='openModal(".$linha['json'].")'><i class='fa fa-search'></i></button>
+                    <a class='btn btn-primary' style='margin-left: 8px, margin-right: 10px;' href='alterarTarefa.php?id=".$linha['id']."'><div class='fa fa-edit'></div></a>
+                    <button class='btn btn-danger' style='margin-left: 8px, margin-right: 10px;'><a class='fa fa-trash' href='../php/excluirTarefa.php?id=".$linha['id']."'></a></button>
                   </center>
                   </td>
             </tr>"; 
@@ -64,5 +64,42 @@ foreach($resultado as $linha){
 		echo"
 		</tbody>
         </table>";
+    
+    echo" <!-- Modal -->
+          <div class='modal fade' id='myModal' tabindex='-1' role='dialog' aria-labelledby='myModalTitle' aria-hidden='true'>
+            <div class='modal-dialog modal-dialog-centered' role='document'>
+              <div class='modal-content'>
+                <div id='modalHeader' class='modal-header'>
+                  <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+                    <span aria-hidden='true'>&times;</span>
+                  </button>
+                </div>
+                <div class='modal-body'>
+                  <div>
+                    <label for='titulo'>Título</label>
+                    <input type='titulo' class='form-control' id='titulo' readonly>
+                    <label for='descricao' style='padding-top: 10px;'>Descrição</label>
+                    <div class='input-group'>
+                    <textarea class='form-control' id='descricao' readonly></textarea>
+                    </div>
+                    <div>
+                    <div style='display: flex;'>
+                      <div style='width: 50%; padding-top: 10px; margin: 10px;'>
+                        <label for='hInicio' style='display: block; text-align: center;'>Hora Início</label>
+                        <input type='hInicio' class='form-control' id='hInicio' style='text-align: center;' readonly>
+                      </div>
+                      <div style='width: 50%; padding-top: 10px; margin: 10px;'>
+                        <label for='hFim' style='display: block; text-align: center;'>Hora Fim</label>
+                        <input type='hFim' class='form-control' id='hFim' style='text-align: center;' readonly>
+                      </div>
+                    </div>
+
+                </div>
+                <div class='modal-footer'>
+                  <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>
+                </div>
+              </div>
+            </div>
+          </div>";
 }
 ?>
