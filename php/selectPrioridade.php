4af3c122286c9cc77666fde16585850a6058668b
zuma <zuamirgutemberg@gmail.com>
2020-10-05 21:32:19 -0300

    feat[US023] Implementa logica para selecionar o valor dos campos prioridade e tipo tarefa

--- php/selectPrioridade.php
+++ php/selectPrioridade.php
@@ -3,22 +3,31 @@
 include 'conexao.php';
 
 $idUsuario = $_SESSION['idusuario'];  
+$idPrioridade = $linha['id_prioridade'];
+$prioridade = $linha['prioridade'];
 
-$stmt = $conexao->prepare("SELECT id,descricao FROM prioridade");
+if ($idPrioridade != null) {
+	$stmt = $conexao->prepare("SELECT id,
+									  descricao
+								 FROM prioridade
+								WHERE id != $idPrioridade");
+} else {
+	$stmt = $conexao->prepare("SELECT id,
+									  descricao
+								 FROM prioridade");
+}
 $stmt->execute();
 
 $resultado = $stmt->fetchAll();
-  
-                            
-echo "<option selected='selected' value='-1'>Selecione</option>"; 
 
-foreach($resultado as $row){ 
-                          
+if($idPrioridade != null){
+	echo "<option selected='selected' value=".$idPrioridade.">".$prioridade."</option>"; 
+} else {
+	echo "<option selected='selected' value='-1'>Selecione</option>";
+}
+
+foreach($resultado as $row){                           
 	echo "<option value=".$row['id'].">".$row['descricao']."</option>"; 	
-
-
-
-
 }	
 
 
