213fd345dc4ea5885682c9c5361ffb10ca753a32
Jobson Ferreira <jobsonraphael@hotmail.com.br>
2020-11-22 22:00:47 -0300

    feat[US015] Remover notificações

---
+++ php/listarNotificacao.php
@@ -0,0 +1,28 @@
+<?php
+include 'conexao.php';
+
+$idUsuario = $_SESSION['idusuario'];
+
+
+$stmt = $conexao->query("select nt.titulo, nt.classificacao, nt.descricao from usuario_notificacao un inner join notificacao nt on nt.id = un.notificacao  where usuario = '$idUsuario'");
+
+$contagem = $stmt->rowCount();
+$resultado = $stmt->fetchAll();
+if($contagem > 0){
+foreach($resultado as $linha){
+
+	echo "<div class='alert alert-warning alert-dismissible fade show' role='alert'>
+			<h5 class='alert-heading'>".$linha['titulo']."</h5>
+			<div>".$linha['descricao']." </div>
+			  <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+			    <span aria-hidden='true'>&times;</span>
+			  </button>
+		  </div>";
+}
+
+}else{
+
+	echo"<div>Não há notificações</div>";
+
+}
+?>
